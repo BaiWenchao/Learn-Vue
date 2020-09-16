ec93ca983fb66c28f1d48b9736d615a6598dfc2f
white <202418628@qq.com>
2020-09-16 23:58:01 +0800

    feat(global): config eslint and prettier

---
+++ app.js
@@ -0,0 +1,2 @@
+var a = 'app'
+console.log(a)
---
+++ test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be single',
+    }
+  },
+}
+</script>
+<style></style>
